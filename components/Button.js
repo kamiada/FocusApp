bc09e88674afdd1f05763cc28c99508dcadb4fbd
Kamiada <kamiada528@gmail.com>
2020-07-11 00:10:21 +0100

    meh

--- components/Button.js
+++ components/Button.js
@@ -3,10 +3,12 @@ import { Text, TouchableOpacity, View, Image } from "react-native";
 import styles from "../styles";
 
 class Button extends React.Component {
+    handlePress = () => {}
     render(){
         return(
             <View style={styles.button}>
-            <TouchableOpacity>
+            <TouchableOpacity onPress={this.handlePress}>
+                <Text>Start</Text>
                 <Image source={require("../assets/Button.png")}/>
             </TouchableOpacity>
           </View>
