1ed0e192e20b172c38551fb1a362af703a862d73
Kamiada <kamiada528@gmail.com>
2020-07-14 23:00:48 +0100

    started Timer

--- components/Timer.js
+++ components/Timer.js
@@ -1,7 +1,17 @@
 import {React} from "react";
 import { TouchableWithoutFeedback, Text } from "react-native";
 import styles from "../styles";
+import { render } from "react-dom";
 
-export default function Timer(){
-    
+export class Timer extends React.Component(){
+    //depending to user input - so props
+    //count down the state
+    //pure functionsd cannot use states
+    render(){        
+    return(
+            <View style={styles.center}>
+                <Text>{this.props.userInput}</Text>
+            </View>
+        );
+    }
 }
